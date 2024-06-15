ff5bc0925aa28f2cf102ff1ca9c29d0d0239d137
kiwiki <jejebainier@gmail.com>
2024-06-15 16:49:00 +0200

    able to scan 2 server type on same ip can chose ports to scan with port ranges print multithread probleme resolved

--- main.py
+++ main.py
@@ -8,21 +8,61 @@ import csv
 import os
 import sqlite3
 
-def scan_ip(ip, verbose, save, csv_filename=None, db_conn=None): #scaner
-    try:
-        #test for java server
-        server = JavaServer.lookup(f"{ip}:25565")
-        status = server.status()
-        server_type = "Java"
-        description = status.description
-        players_online = status.players.online
-        players_max = status.players.max
-        version = status.version.name
-        latency = status.latency
-    except Exception:
+def scan_ip(ip, java_ports, bedrock_ports, verbose, save, csv_filename=None, db_conn=None): #scaner
+    for port in java_ports:
+        try:
+            #test for java server
+            server = JavaServer.lookup(f"{ip}:{port}")
+            status = server.status()
+            server_type = "Java"
+            description = status.description
+            players_online = status.players.online
+            players_max = status.players.max
+            version = status.version.name
+            latency = status.latency
+            prt = ""
+            if verbose >= 1:
+                prt += "------------------------------------------------------- \n"
+                prt +=f"Server found at {ip}:{port} \n"
+                prt +=f"Type: {server_type} \n"
+                prt +=f"Description: {description} \n"
+                prt +=f"Players: {players_online}/{players_max} \n"
+                prt +=f"Version: {version} \n"
+                prt +=f"Latency: {latency} ms"
+                print(prt)
+
+            if save == 'csv' and csv_filename: #if save -> to csv or db
+                with open(csv_filename, mode='a', newline='') as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    csv_writer.writerow([
+                        ip, port, server_type,
+                        description,
+                        players_online,
+                        players_max,
+                        version,
+                        latency
+                    ])
+            elif save == 'sqlite' and db_conn:
+                cursor = db_conn.cursor()
+                cursor.execute(
+                    "INSERT INTO servers (ip, port, type, description, players_online, max_players, version, latency) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                    (ip, port, server_type,
+                     description,
+                     players_online,
+                     players_max,
+                     version,
+                     latency)
+                )
+                db_conn.commit()
+            return #Java server found, no need to check other ports
+        except Exception:
+            if verbose >= 2:
+                print(f"No Java server at {ip}:{port}")
+
+    for port in bedrock_ports:
         try:
             #test for bedrock server
-            server = BedrockServer.lookup(f"{ip}:19132")
+            server = BedrockServer.lookup(f"{ip}:{port}")
             status = server.status()
             server_type = "Bedrock"
             description = status.motd.raw
@@ -30,43 +70,44 @@ def scan_ip(ip, verbose, save, csv_filename=None, db_conn=None): #scaner
             players_max = status.players_max
             version = status.version.version
             latency = "N/A"
+            prt = ""
+            if verbose >= 1:
+                prt += "------------------------------------------------------- \n"
+                prt +=f"Server found at {ip}:{port} \n"
+                prt +=f"Type: {server_type} \n"
+                prt +=f"Description: {description} \n"
+                prt +=f"Players: {players_online}/{players_max} \n"
+                prt +=f"Version: {version} \n"
+                prt +=f"Latency: {latency} ms"
+                print(prt)
+
+            if save == 'csv' and csv_filename: #if save -> to csv or db
+                with open(csv_filename, mode='a', newline='') as csv_file:
+                    csv_writer = csv.writer(csv_file)
+                    csv_writer.writerow([
+                        ip, port, server_type,
+                        description,
+                        players_online,
+                        players_max,
+                        version,
+                        latency
+                    ])
+            elif save == 'sqlite' and db_conn:
+                cursor = db_conn.cursor()
+                cursor.execute(
+                    "INSERT INTO servers (ip, port, type, description, players_online, max_players, version, latency) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                    (ip, port, server_type,
+                     description,
+                     players_online,
+                     players_max,
+                     version,
+                     latency)
+                )
+                db_conn.commit()
+            return #Bedrock server found, no need to check other ports
         except Exception:
             if verbose >= 2:
-                print(f"No server at {ip}:25565 or {ip}:19132")
-            return
-
-    if verbose >= 1:
-        print("-------------------------------------------------------")
-        print(f"Server found at {ip}:{'25565' if server_type == 'Java' else '19132'}")
-        print(f"Type: {server_type}")
-        print(f"Description: {description}")
-        print(f"Players: {players_online}/{players_max}")
-        print(f"Version: {version}")
-        print(f"Latency: {latency} ms")
-
-    if save == 'csv' and csv_filename:  #if save -> to csv or db
-        with open(csv_filename, mode='a', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow([
-                ip, server_type,
-                description,
-                players_online,
-                players_max,
-                version,
-                latency
-            ])
-    elif save == 'sqlite' and db_conn:
-        cursor = db_conn.cursor()
-        cursor.execute(
-            "INSERT INTO servers (ip, type, description, players_online, max_players, version, latency) VALUES (?, ?, ?, ?, ?, ?, ?)",
-            (ip, server_type,
-             description,
-             players_online,
-             players_max,
-             version,
-             latency)
-        )
-        db_conn.commit()
+                print(f"No Bedrock server at {ip}:{port}")
 
 def generate_ips(ip_ranges): #generate IP range according to --ip
     for ip_range in ip_ranges:
@@ -88,11 +129,21 @@ def parse_ip_ranges(ip_ranges): #parse if single param or multiple param
             ranges.append((ip_range.strip(), ip_range.strip()))
     return ranges
 
-def worker(ips, verbose, save, csv_filename=None, db_file=None, delay=0): #multi thread worker
+def parse_ports(port_ranges): #parse port ranges
+    ports = set()
+    for port_range in port_ranges.split(','):
+        if '-' in port_range:
+            start_port, end_port = map(int, port_range.split('-'))
+            ports.update(range(start_port, end_port + 1))
+        else:
+            ports.add(int(port_range.strip()))
+    return sorted(ports)
+
+def worker(ips, java_ports, bedrock_ports, verbose, save, csv_filename=None, db_file=None, delay=0): #multi thread worker
     pool = gevent.pool.Pool(size=1000)
     db_conn = sqlite3.connect(db_file) if db_file else None
     for ip in ips:
-        pool.spawn(scan_ip, ip, verbose, save, csv_filename, db_conn)
+        pool.spawn(scan_ip, ip, java_ports, bedrock_ports, verbose, save, csv_filename, db_conn)
         if delay > 0:
             gevent.sleep(delay / 1000) #add delay to avoid ban or yolo mode if 0
     pool.join()
@@ -105,6 +156,8 @@ def main():
     parser.add_argument('--verbose', type=int, default=1, help="Verbosity level (0, 1, 2)")
     parser.add_argument('--save', type=str, default=None, help="Save found servers to a CSV file or a database (sqlite, postgres, mysql)")
     parser.add_argument('--ip', type=str, required=True, help="IP ranges to scan (e.g., '192.168.0.0-192.168.255.255,192.168.0.0-192.168.0.255')")
+    parser.add_argument('--java-ports', type=str, default="25565", help="Ports to scan for Java servers (e.g., '25565,25566-25570')")
+    parser.add_argument('--bedrock-ports', type=str, default="19132", help="Ports to scan for Bedrock servers (e.g., '19132,19133-19135')")
     parser.add_argument('--db-host', type=str, help="Database host (for postgres, mysql)")
     parser.add_argument('--db-port', type=str, help="Database port (for postgres, mysql)")
     parser.add_argument('--db-user', type=str, help="Database user (for postgres, mysql)")
@@ -124,13 +177,14 @@ def main():
         if not os.path.exists(csv_filename):
             with open(csv_filename, mode='w', newline='') as csv_file:
                 csv_writer = csv.writer(csv_file)
-                csv_writer.writerow(["IP", "Type", "Description", "Players Online", "Max Players", "Version", "Latency"])
+                csv_writer.writerow(["IP", "Port", "Type", "Description", "Players Online", "Max Players", "Version", "Latency"])
     elif args.save == 'sqlite': #init db
         db_conn = sqlite3.connect(db_filename)
         db_conn.execute(
             "CREATE TABLE IF NOT EXISTS servers ("
             "id INTEGER PRIMARY KEY AUTOINCREMENT,"
             "ip TEXT,"
+            "port INTEGER,"
             "type TEXT,"
             "description TEXT,"
             "players_online INTEGER,"
@@ -141,6 +195,8 @@ def main():
         db_conn.close()
 
     ip_ranges = parse_ip_ranges(args.ip)
+    java_ports = parse_ports(args.java_ports)
+    bedrock_ports = parse_ports(args.bedrock_ports)
     ip_list = list(generate_ips(ip_ranges))
     chunk_size = len(ip_list) // multiprocessing.cpu_count() #share work between cpu
 
@@ -148,7 +204,7 @@ def main():
     for i in range(multiprocessing.cpu_count()):
         start = i * chunk_size
         end = len(ip_list) if i == multiprocessing.cpu_count() - 1 else (i + 1) * chunk_size
-        p = multiprocessing.Process(target=worker, args=(ip_list[start:end], args.verbose, args.save, csv_filename, db_filename, args.delay))
+        p = multiprocessing.Process(target=worker, args=(ip_list[start:end], java_ports, bedrock_ports, args.verbose, args.save, csv_filename, db_filename, args.delay))
         processes.append(p)
         p.start()
 
